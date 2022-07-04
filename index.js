1162ccc1df33043c67e20891800884f985599b3d
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-03 22:41:03 -0400

    add parameters

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const PORT = 5000;
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
+
+const url = 'https://www.theguardian.com/us'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = [];
+
+        $('.fc-item__header', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        console.log(articles);
+    }).catch(err => console.log(err));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
 
